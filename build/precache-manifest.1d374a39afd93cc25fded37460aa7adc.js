77a577c5fb82ff783c65714c5529eae53262146e
Andrei Bostan <abostan.ir@gmail.com>
2020-05-04 16:52:10 +0200

    ready for heroky

---
+++ build/precache-manifest.1d374a39afd93cc25fded37460aa7adc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f201da6bdb877a9435c84ecf5451cb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ada356bccc12e0e2fae",
+    "url": "/static/css/main.9b07aca2.chunk.css"
+  },
+  {
+    "revision": "86a9ec428dca51710652",
+    "url": "/static/js/2.76d852fb.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/static/js/2.76d852fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ada356bccc12e0e2fae",
+    "url": "/static/js/main.d26bc6e9.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  }
+]);
