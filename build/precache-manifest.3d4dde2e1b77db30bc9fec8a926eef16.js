2642b3d8652bf3a86b64a681ea2dc52b1395b57a
Andrei Bostan <abostan.ir@gmail.com>
2020-09-08 11:55:20 +0200

    moved build

---
+++ build/precache-manifest.3d4dde2e1b77db30bc9fec8a926eef16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f89d1e600c23ca135718162ed26e95c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a8baf9a21ca83791ac9",
+    "url": "/static/css/main.5c31d214.chunk.css"
+  },
+  {
+    "revision": "923e5afcb5dd3ab8c758",
+    "url": "/static/js/2.d1de00ab.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/static/js/2.d1de00ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a8baf9a21ca83791ac9",
+    "url": "/static/js/main.80686a03.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
